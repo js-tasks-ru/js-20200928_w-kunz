9182bd91481d5a19ce11a902a8bd332b4c436629
Waldemar Kunz <w_kunz@web.de>
2020-10-07 14:48:54 +0200

    Lesson 03 Exercises 01-03 solved

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,14 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  return obj => {
+    if (typeof path !== 'string' || path === '') return undefined
+    const properties = path.split('.')
+    let tempProperty = {...obj}
+    while (properties.length) {
+      if (!tempProperty[properties[0]]) return undefined
+      tempProperty = tempProperty[properties.shift()]
+    }
+    return tempProperty
+  }
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,17 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  if (string === '') return ''
+  const arrayOfSubstrings = []
+  let tempString = string[0]
+  for (let i = 1; i < string.length; i++) {
+    if (string[i] === tempString[0]) {
+      tempString += string[i]
+    } else {
+      arrayOfSubstrings.push(tempString)
+      tempString = string[i]
+    }
+  }
+  arrayOfSubstrings.push(tempString)
+  return arrayOfSubstrings.reduce((result, subString) => result += subString.slice(0,size),'')
 }
